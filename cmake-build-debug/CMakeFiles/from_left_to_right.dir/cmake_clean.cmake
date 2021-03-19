3c467f1d9099483f8346d474c25228d8047ee866
Bystrykha <59131326+Bystrykha@users.noreply.github.com>
2021-03-19 23:25:02 +0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/from_left_to_right.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/from_left_to_right.dir/main.cpp.obj"
+  "from_left_to_right.exe"
+  "from_left_to_right.exe.manifest"
+  "from_left_to_right.pdb"
+  "libfrom_left_to_right.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/from_left_to_right.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
